f7cd083cd8047f4f4ff7b704b5f9564b777b75c3
Jason <Jason.Forral@gmail.com>
2019-12-09 14:59:36 -0800

    created files

---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PrimeService
+{
+    public class PrimeService
+    {
+    public bool isPrime(int candiadate) => throw new NotImplementedException("Not Implemented.");
+  }
+}
